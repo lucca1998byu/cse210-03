84cf1a9a3697535cf0f915cd1b4556c3bfbd9b24
anuchaj <anuchaj50@gmail.com>
2022-02-11 19:02:50 +0100

    Added comments

--- game/check.py
+++ game/check.py
@@ -2,11 +2,13 @@
 #If the guess is correct, the letter is revealed.
 #If the guess is incorrect, a line is cut on the player's parachute.
 
+# defines the Check() class.
 class Check():
     def __init__(self):
         self.word = ""
         self.display = []
 
+    # the check method takes four arguement.
     def check(self, guess, word, display ):
         self.word = word
         self.display = display
